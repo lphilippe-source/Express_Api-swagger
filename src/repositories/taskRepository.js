9b3bb56f84c6755582a09dc78df5ccc0bc612983
lphilippe-source <philippelmr@gmail.com>
2021-05-27 15:46:10 +0200

    fix: :bug: duplicate
    diplicate js with tsc

--- src/repositories/taskRepository.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TaskRepository = void 0;
-class TaskRepository {
-    getUserTasks(user) { }
-}
-exports.TaskRepository = TaskRepository;
--- src/services/demoService.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.demoService = void 0;
-class DemoService {
-    add(x, y) {
-        return x + y;
-    }
-}
-exports.demoService = Object.freeze(new DemoService());
